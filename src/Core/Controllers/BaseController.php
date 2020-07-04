c7b06e1d90199e294cff4a8bcbc70a78bff37deb
Jeffrey <jeffrey.mcmullen@yahoo.com>
2020-07-04 14:37:01 -0400

    Move base controller into its own directory

--- src/Core/Controllers/BaseController.php
+++ src/Core/Controllers/BaseController.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace App\Core;
+namespace App\Core\Controllers;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
